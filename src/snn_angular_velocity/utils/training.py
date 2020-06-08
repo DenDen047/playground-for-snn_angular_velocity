9d1ac5963d239808f3403f09e8ec034886572ba5
DenDen047 <sh.mn.nat@gmail.com>
2020-06-08 22:33:11 +0900

    success to run backward()

--- src/snn_angular_velocity/utils/training.py
+++ src/snn_angular_velocity/utils/training.py
@@ -9,6 +9,8 @@ from model.metric import medianRelativeError, rmse
 from .gpu import moveToGPUDevice
 from .tbase import TBase
 
+from model.loss import compute_loss
+
 
 class Trainer(TBase):
     def __init__(self, data_dir, write, log_config, general_config):
@@ -31,6 +33,8 @@ class Trainer(TBase):
         self._loadNetFromCheckpoint()
         self.net = self.net.train()
 
+        optimizer = torch.optim.Adam(self.net.parameters(), lr = 0.01, amsgrad = True)
+
         for data in tqdm(self.train_loader, desc='training'):
             data = moveToGPUDevice(data, self.device, self.dtype)
 
@@ -39,6 +43,14 @@ class Trainer(TBase):
 
             ang_vel_pred = self.net(spike_tensor)
             self.data_collector.append(ang_vel_pred, ang_vel_gt, data['file_number'])
+
+            loss = compute_loss(ang_vel_pred, ang_vel_gt)
+
+            optimizer.zero_grad()
+            loss.backward()
+
+            optimizer.step()
+
         if self.write_output:
             self.data_collector.writeToDisk(self.output_dir)
         self.data_collector.printErrors()
